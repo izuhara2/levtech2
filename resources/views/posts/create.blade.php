a21cddb842770d536ee2c09ffcbdda11e20e351f
izuhara2 <izuharasif@outlook.jp>
2022-04-02 13:17:41 +0900

    バリデーション実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,11 +16,13 @@
             {{ csrf_field() }}
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="title">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="記事を入力してください"></textarea>　
+                <textarea name="post[body]" placeholder="文章を入力して下さい。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="store"/> 
         </form>
