f2edc1f0e1b01da8dc813235e0c47ad9945b847a
izuhara2 <izuharasif@outlook.jp>
2022-04-17 14:12:42 +0900

    カリキュラム9-2

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,10 @@ Route::put('/posts/{post}', 'PostController@update');
 Route::delete('/posts/{post}', 'PostController@delete');
 
 Route::get('/categories/{category}', 'CategoryController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
